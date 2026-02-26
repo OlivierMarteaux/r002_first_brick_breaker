d02693a95a46dd9456a1ab03c760799ebdcd3682
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-02-26 12:28:33 +0100

    01_20260226_OM: updated Firebase config

--- src/data/firebase.ts
+++ src/data/firebase.ts
@@ -1,15 +1,8 @@
 import { initializeApp, FirebaseApp } from "firebase/app";
 import { getAuth, Auth } from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
+import { firebaseConfig } from '../../secrets/firebaseConfig';
+import { getAnalytics } from "firebase/analytics";
 
 let app: FirebaseApp | undefined;
 let auth: Auth | undefined;
@@ -21,6 +14,7 @@ const isConfigured = firebaseConfig.apiKey && firebaseConfig.apiKey.length > 10;
 if (isConfigured) {
   try {
     app = initializeApp(firebaseConfig);
+	const analytics = getAnalytics(app);
     auth = getAuth(app);
     db = getFirestore(app);
   } catch (error) {
